fd74eaba278faad80ea5bee0a13c3717d48977d2
yvonnkr <yvonnkr86@gmail.com>
2024-11-21 15:32:59 +0000

    add author and startup sanity schemas

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import React from "react";
+import "easymde/dist/easymde.min.css";
 
 const workSans = localFont({
     src: [
