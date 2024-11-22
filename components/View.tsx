3b2e5ea7e9a068f4a0f0a50f87cff29243f7d8ce
yvonnkr <yvonnkr86@gmail.com>
2024-11-22 22:34:10 +0000

    implement sanity write client

--- components/View.tsx
+++ components/View.tsx
@@ -2,13 +2,21 @@ import React from "react";
 import Ping from "@/components/Ping";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
+import { unstable_after as after } from "next/server";
 
 const View = async ({ id }: { id: string }) => {
     const { views: totalViews } = await client
         .withConfig({ useCdn: false })
         .fetch(STARTUP_VIEWS_QUERY, { id });
 
-    //TODO: Update the number of views
+    after(
+        async () =>
+            await writeClient
+                .patch(id)
+                .set({ views: totalViews + 1 })
+                .commit()
+    );
 
     return (
         <div className="view-container">
--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -4,11 +4,10 @@
 import "server-only";
 import { defineLive } from "next-sanity";
 import { client } from "./client";
+import { token } from "@/sanity/env";
 
 export const { sanityFetch, SanityLive } = defineLive({
-    client: client.withConfig({
-        // Live content is currently only available on the experimental API
-        // https://www.sanity.io/docs/api-versioning
-        apiVersion: "vX",
-    }),
+    client,
+    serverToken: token,
+    browserToken: token,
 });
