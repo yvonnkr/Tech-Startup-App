2abe07049f3a6b897347891b3c18529b2837419b
yvonnkr <yvonnkr86@gmail.com>
2024-11-23 23:11:52 +0000

    implement author auth flow

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,7 @@ import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { auth } from "@/auth";
 
 export default async function Home({
     searchParams,
@@ -10,6 +11,9 @@ export default async function Home({
 }) {
     const query = (await searchParams).query;
     const params = { search: query || null };
+    const session = await auth();
+    console.log(`session-id: ${session?.id}`);
+
     const { data: posts } = await sanityFetch({
         query: STARTUPS_QUERY,
         params: params,
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -34,3 +34,15 @@ export const STARTUP_VIEWS_QUERY = defineQuery(`
         _id, views
     }
 `);
+
+export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`
+*[_type == "author" && id == $id][0]{
+    _id,
+    id,
+    name,
+    username,
+    email,
+    image,
+    bio
+}
+`);
